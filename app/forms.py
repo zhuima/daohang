dd9e7b35d1f1fc889944c6f7653a7310331f60c1
edz <edz@edzdeMacBook-Air-8.local>
2019-03-12 14:09:58 +0800

    添加自助提交链接页面

---
+++ app/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+
+from app.models import UrlInfor
+from django.utils.http import is_safe_url
+
+
+class CommitForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(CommitForm, self).__init__(*args, **kwargs)
+        self.fields['url_name'].help_text = '链接名称，例如：wiki.'
+        self.fields['url_path'].help_text = '链接，例如：https://zh.wikipedia.org/zh/Wiki.'
+        self.fields['url_desc'].help_text = '链接描述:，例如：这是wiki链接，你可以通过这个链接了解相关技术。'
+
+    class Meta:
+        model = UrlInfor
+        fields = ['url_name', 'url_path', 'url_group', 'url_desc']
+
+    def clean_url_name(self):
+        url_name = self.cleaned_data.get('url_name')
+        exists = UrlInfor.objects.filter(url_name=url_name).exists()
+        if exists:
+            raise forms.ValidationError(message="%s 已经存在了" % url_name)
+        else:
+            return url_name
+    
+
+    def clean_url_path(self):
+        url_path = self.cleaned_data.get('url_path')
+        exists = is_safe_url(url_path)
+        if exists:
+            raise forms.ValidationError(message="%s url不合法" % url_path)
+        else:
+            return url_path
